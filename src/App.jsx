e3ce2fcfff9dee95306c76d4bc723c51a5e71cba
davneesh <avneeshd09@gmail.com>
2024-12-07 23:54:53 +0530

    add files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import './App.css'
+import React from "react";
+import Home from "./components/Home";
+import About from "./components/About";
+import Project from "./components/Project";
+import Contact from "./components/Contact";
+
+
 
 function App() {
   
  return (
     <>
-      
-       
+
+    <div className="w-screen relative z-0">
+      <Home/>
+    </div>
+    <div>
+      <About/>
+    </div>
+    <div>
+      <Project/>
+    </div>
+    <div>
+      <Contact/>
+    </div>
+    
     </>
   )
 }
